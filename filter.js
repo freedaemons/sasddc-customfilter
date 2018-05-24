ea98ef3ea648ac52a085a89fbaa7e3a8bd8529aa
Friedemann Ang <friedemann.ang@gmail.com>
2018-05-24 11:27:40 +0800

    Debugging contents of casData.data

--- filter.js
+++ filter.js
@@ -24,27 +24,44 @@ function updateData(casData) {
 	var findParents = new Array();
 	var findChildren = new Array();
 	//Replicate the parent OR child parameterized filter in VA
-	for (p = 0; p < casData.data.length; p++) {
-        if (String(casData.data[p][1]).toUpperCase().includes(company).toUpperCase()) {
-            result.push({
-                row: p
-            });
-        }
-        if (String(casData.data[p][5]).toUpperCase().includes(company).toUpperCase()) {
-            result.push({
-                row: p
-            });
-        }
-    };
+	// for (p = 0; p < casData.data.length; p++) {
+ //        if (String(casData.data[p][1]).toUpperCase().includes(company).toUpperCase()) {
+ //            result.push({
+ //                row: p
+ //            });
+ //        }
+ //        if (String(casData.data[p][5]).toUpperCase().includes(company).toUpperCase()) {
+ //            result.push({
+ //                row: p
+ //            });
+ //        }
+ //    };
  	// for (p = 0; p < casData.data.length; p++) { // push everything
   //           result.push({
   //               row: p 
   //           })
   //   }
     
-    // result.push({
-    // 	row: 2 // simulate filter result only containing 3rd row
-    // })
+    result.push({
+    	row: 2 // simulate filter result only containing 3rd row
+    })
+    console.log('--- filter debugging begins ---\n')
+    console.log(String(casData.data[0][0]))
+    console.log(String(casData.data[0][1]))
+    console.log(String(casData.data[0][2]))
+    console.log(String(casData.data[0][3]))
+
+    console.log(String(casData.data[1][0]))
+    console.log(String(casData.data[1][1]))
+    console.log(String(casData.data[1][2]))
+    console.log(String(casData.data[1][3]))
+
+    console.log(String(casData.data[2][0]))
+    console.log(String(casData.data[2][1]))
+    console.log(String(casData.data[2][2]))
+    console.log(String(casData.data[2][3]))
+    console.log('--- filter debugging ends ---\n')
+
     self.resultName = casData.resultName;
     var message = {
         selections: result,
